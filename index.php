af515221e65288057cc98299ae6bbab1ac689dc0
Fettabachi <timfettermail@gmail.com>
2014-07-06 14:11:03 -0400

    Expanding the content

--- index.php
+++ index.php
@@ -19,7 +19,9 @@
           <?php include "_/components/php/article-aboutthevenue.php"; ?>
 				</section><!-- main -->
 				<section class="sidebar col col-lg-4">
-
+          <?php include "_/components/php/aside-register.php"; ?>
+          <?php include "_/components/php/aside-lastyear.php"; ?>
+          <?php include "_/components/php/aside-accordion.php"; ?>
 				</section><!-- sidebar -->
 			</div><!-- content -->
     <?php include "_/components/php/footer.php"; ?>
